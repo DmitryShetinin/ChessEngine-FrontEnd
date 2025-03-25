53cb5e2e554e5fa7f99f80344da4049746e49c18
HateLove <shchetinin-02@list.ru>
2025-03-25 13:06:32 +0300

    create board

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import './App.css';
+import Board  from '../pages/board.tsx';
+
+function App() {
+  return (
+    <Board/>
+  );
+}
+
+export default App;
---
+++ src/pages/board.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css';
+import Gridwrapper from '../widgets/GridWrapper/gridwrapper.tsx'
+
+
+
+const Board = () => (
+    <>
+      <Gridwrapper/>
+    </>
+);
+
+export default Board;
